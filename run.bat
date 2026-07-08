840cd2ad1d8ec8a06e3ce71e9cfbf8e83f496886
Ryuu Mitsuki <dhefam31@proton.me>
2026-07-08 23:18:56 +0700

    build: Separate setup script to `scripts` folder
    This include both Windows (.bat) and Unix (.sh) files, they will now use
    `setup.bat` and `setup.sh` before running application, respectively.

--- run.bat
+++ run.bat
@@ -1,17 +1,15 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-title CYPY - Cyrene's Python Manga Translator
-echo ========================================
-echo   Setting up CYPY...
-echo ========================================
-echo.
-
 REM Script directory
 set "ROOT_DIR=%~dp0"
 set "VENV="
 set "PYTHON="
 set "PYVER="
+
+call !ROOT_DIR!\scripts\setup.bat
+
+title CYPY - Cyrene's Python Manga Translator
 
 REM ----------------------------------------
 REM Detect Python / virtual environment
@@ -42,70 +40,6 @@ if defined VENV (
     set "PYTHON=python"
 )
 
-REM Validate Python version
-"%PYTHON%" -c "import sys; sys.exit(0 if sys.version_info >= (3,8) else 1)"
-if errorlevel 1 (
-    echo [!!] Current Python version is not supported. Need Python 3.8+.
-    exit /b 1
-)
-
-for /f "delims=" %%v in ('"%PYTHON%" --version 2^>^&1') do set "PYVER=%%v"
-
-if defined VENV (
-    echo [+] Using !PYVER! from virtual environment
-) else (
-    echo [+] Using !PYVER! from system Python
-)
-
-REM ----------------------------------------
-REM Setup virtual environment
-REM ----------------------------------------
-
-if not defined VENV (
-    echo [+] Setting up venv for the application...
-    "%PYTHON%" -m venv "%ROOT_DIR%\venv"
-    if errorlevel 1 (
-        echo [!!] Failed to create virtual environment.
-        exit /b 1
-    )
-
-    set "VENV=%ROOT_DIR%\venv"
-    set "PYTHON=!VENV!\Scripts\python.exe"
-
-    echo [+] Upgrading pip...
-    "%PYTHON%" -m pip install --upgrade pip
-    if errorlevel 1 (
-        echo [!!] Failed to upgrade pip.
-    )
-
-    echo [+] Installing Python dependencies...
-    "%PYTHON%" -m pip install -e .
-    if errorlevel 1 (
-        echo [!!] Failed to install dependencies.
-        exit /b 1
-    )
-)
-
-REM ----------------------------------------
-REM Setup .env
-REM ----------------------------------------
-
-if not exist ".env" (
-    echo [i] No .env found. Copying from .env.example...
-
-    if exist ".env.example" (
-        copy ".env.example" ".env" >nul
-        echo [+] .env created. Please edit it with your API key.
-
-        if defined EDITOR (
-            "%EDITOR%" ".env"
-        ) else (
-            notepad ".env"
-        )
-    ) else (
-        echo [!!] No .env.example found. Please create .env manually.
-    )
-)
 
 REM ----------------------------------------
 REM Run application
